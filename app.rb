294e465a892fba7c3f7014808b86a41d23f95504
James Alexander <james@jmslxndr.com>
2015-09-10 15:29:22 -0400

    Added login, changed app.rb, multiple other changes...

--- app.rb
+++ app.rb
@@ -5,7 +5,49 @@ require 'rack-flash'
 require './models'
 
 configure(:development) { set :database, "sqlite3:login.sqlite3" }
+set :sessions, true
+
+def current_user
+	if session[:user_id]
+		User.find(session[:user_id])
+	else
+		nil
+	end
+end
 
 get '/' do 
 	erb :home
 end
+
+get '/settings' do 
+	erb :settings
+end
+
+get '/layout' do 
+	erb :layout
+end
+
+get '/profile' do 
+	erb :profile
+end
+
+get '/signup' do 
+	erb :signup
+end
+
+post '/sessions' do 
+	user = User.find_by(email: params[:email])
+	if user and user.password == params['password']
+		puts "Logged in successfully!"
+		session[:user_id] = user.id 
+		redirect to '/'
+	else
+		puts "Invalid Email or Password"
+		redirect to '/login'
+	end
+end
+
+get '/login' do 
+	erb :login
+end
+
