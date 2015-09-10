00d50e86dda4424353c7992d5a2d08d87a9b2eb3
James Alexander <james@jmslxndr.com>
2015-09-10 18:00:46 -0400

    Everything

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150910181501) do
+ActiveRecord::Schema.define(version: 20150910210026) do
 
   create_table "posts", force: :cascade do |t|
     t.string  "body"
     t.integer "user_id"
+  end
+
+  create_table "user_posts", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
   end
 
   create_table "users", force: :cascade do |t|
